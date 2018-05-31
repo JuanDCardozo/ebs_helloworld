242f150aa9edddfb9385544c61c4a77d231b1d7b
Juan D. Cardozo <jcardozo@ople.ai>
2018-05-31 13:42:03 -0700

    Updated logging formating

--- application.py
+++ application.py
@@ -6,9 +6,6 @@ import logging
 def say_hello(username = "World"):
     logger = logging.getLogger()
     handler = logging.StreamHandler()
-    formatter = logging.Formatter(
-            '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
-    handler.setFormatter(formatter)
     logger.addHandler(handler)
     logger.setLevel(logging.DEBUG)
 
